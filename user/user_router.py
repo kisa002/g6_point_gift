bb8984b60fc6e78475ef81c1a4cf6a24ae3b5314
Holy-Knight <vnycall74@naver.com>
2024-01-23 03:32:41 +0900

    feat: design and add exception case

--- user/user_router.py
+++ user/user_router.py
@@ -1,22 +1,20 @@
+import select
 from fastapi import APIRouter, Form
 from starlette.requests import Request
 from starlette.responses import RedirectResponse
 
+from core.exception import AlertException
+from core.database import db_session
 from core.template import UserTemplates
+
 from .. import plugin_config
-from ..plugin_config import module_name
+from ..plugin_config import router_prefix, module_name
 
 from lib.point import insert_point, delete_point
+from lib.common import Member, select
 
 router = APIRouter()
-
 templates = UserTemplates()
-
-
-@router.get("/point_gift_json")
-async def show(request: Request):
-    """json 출력예시"""
-    return {"message": "Hello POINT GIFT JSON!"}
 
 
 @router.get("/point_gift")
@@ -33,12 +31,29 @@ async def show(request: Request):
         })
 
 @router.post("/point_gift")
-async def gift_point(request: Request, receiver_id: str = Form(...), point: int= Form(...)):
+async def gift_point(
+    request: Request,
+    db: db_session,
+    receiver_id: str = Form(...),
+    point: int= Form(...)
+):
     """포인트 선물"""
-    # delete_point(requset, request.state.login_member.mb_id, point, request.state.login_member.mb_id + "님에게 포인트 선물", "", "", "")
-    
     sender_id = request.state.login_member.mb_id
-    print("sender:", sender_id, "receiver:", receiver_id, "point:", point)
+    current_point = request.state.login_member.mb_point
+    
+    # 포인트를 받을 회원이 존재하는지 확인
+    is_exists = db.scalar(
+        select(Member).where(
+            Member.mb_id == receiver_id,
+        )
+    )
+    
+    if not is_exists:
+        raise AlertException(detail = "포인트를 받을 회원을 찾을 수 없습니다.", status_code=404, url="point_gift")
+    if sender_id == receiver_id:
+        raise AlertException(detail = "자기 자신에게 포인트를 선물할 수 없습니다.", status_code=400, url="point_gift")
+    if current_point < point:
+        raise AlertException(detail = "포인트가 부족합니다.", status_code=400, url="point_gift")
     
     # 자신의 포인트 차감
     insert_point(request, receiver_id, point, sender_id + "님에게 포인트 선물 받음", "", "", "")
@@ -46,12 +61,4 @@ async def gift_point(request: Request, receiver_id: str = Form(...), point: int=
     # 상대방의 포인트 증가
     insert_point(request, sender_id, -point, receiver_id + "님에게 포인트 선물 보냄", "", "", "")
     
-    return templates.TemplateResponse(
-        f"{plugin_config.TEMPLATE_PATH}/user_demo.html",
-        {
-            "request": request,
-            "title": "포인트 선물!",
-            "point": request.state.login_member.mb_point,
-            "action_url": "point_gift/",
-            "content": f"Hello {module_name}!",
-        })
+    return RedirectResponse(url=f"{module_name}", status_code=303)
